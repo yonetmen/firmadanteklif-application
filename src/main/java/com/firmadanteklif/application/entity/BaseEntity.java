983804cf2b4bda1ecb6a0dda60e609e01ea174f7
Kasim Gul <kasimgul@gmail.com>
2019-03-27 21:56:42 +0100

    Adding missing import statement

--- src/main/java/com/firmadanteklif/application/entity/BaseEntity.java
+++ src/main/java/com/firmadanteklif/application/entity/BaseEntity.java
@@ -7,10 +7,7 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
