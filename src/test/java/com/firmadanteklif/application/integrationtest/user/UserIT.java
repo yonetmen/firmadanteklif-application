0fc35438a57521f84e87140e03355dcbab40f54d
Kasim Gul <kasimgul@gmail.com>
2019-04-28 12:36:03 +0200

    Setup & adding new tests

--- src/test/java/com/firmadanteklif/application/integrationtest/user/UserIT.java
+++ src/test/java/com/firmadanteklif/application/integrationtest/user/UserIT.java
@@ -2,7 +2,12 @@ package com.firmadanteklif.application.integrationtest.user;
 
 import com.firmadanteklif.application.Application;
 import com.firmadanteklif.application.entity.SiteUser;
+import com.firmadanteklif.application.entity.enums.VerificationEvent;
+import com.firmadanteklif.application.entity.pojo.VerificationMessage;
 import com.firmadanteklif.application.repository.UserRepository;
+import com.firmadanteklif.application.service.UserService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -21,16 +27,56 @@ import static org.junit.Assert.assertNotNull;
 public class UserIT {
 
     @Autowired
+    protected UserService userService;
+
+    @Autowired
     protected UserRepository userRepository;
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void destroy() {
+        userRepository.deleteAll();
+    }
 
     @Test
     public void createUserTest() {
-        SiteUser user = new SiteUser();
-        user.setEmail("kasim@gmail.com");
-        user.setPassword("kasim123");
+        final String email = "kasimgul@gmail.com";
+        final SiteUser user = createBasicUser(email);
 
-        SiteUser created = userRepository.save(user);
-        System.out.println("USER ID: " + created.getUuid());
+        SiteUser created = userService.register(user);
         assertNotNull(created.getUuid());
+    }
+
+    @Test
+    public void generateActivationNeededMessageTest() {
+        final String email = "kasimgul@gmail.com";
+        VerificationMessage message = userService.generateActivationNeededMessage(email);
+        assertEquals(message.getEvent(), VerificationEvent.REGISTER.name());
+        assertEquals("Giris yapmadan önce Email adresinizi onaylamaniz gerekiyor", message.getMessage());
+    }
+
+    @Test
+    public void getUserTest() {
+        final String email = "kasimgul@gmail.com";
+        final SiteUser user = createBasicUser(email);
+
+        SiteUser created = userService.register(user);
+        SiteUser fetched = userService.getUser(created);
+
+        assertEquals(email, fetched.getEmail());
+    }
+
+
+    /***********     Helper Methods      ***********/
+
+    private SiteUser createBasicUser(String email) {
+        SiteUser user = new SiteUser();
+        user.setEmail(email);
+        user.setPassword("ksm123");
+        return user;
     }
 }
--- src/test/java/com/firmadanteklif/application/unittest/user/UserTest.java
+++ src/test/java/com/firmadanteklif/application/unittest/user/UserTest.java
@@ -1,34 +1,51 @@
-package com.firmadanteklif.application.integrationtest.user;
+package com.firmadanteklif.application.unittest.user;
 
 import com.firmadanteklif.application.entity.SiteUser;
 import com.firmadanteklif.application.repository.UserRepository;
+import com.firmadanteklif.application.security.UserDetailsServiceImpl;
+import com.firmadanteklif.application.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserRepository.class)
+@ContextConfiguration(classes = {UserDetailsServiceImpl.class})
 public class UserTest {
 
     @MockBean
     private UserRepository userRepository;
 
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void registerNewUserTest() {
+        final String email = "kasimgul@gmail.com";
+        final SiteUser user = createBasicUser(email);
+
+        Mockito.when(userService.register(user))
+                .thenReturn(user);
+
+        SiteUser siteUser = userService.register(user);
+        assertEquals(email, siteUser.getEmail());
+    }
+
     @Test
     public void createUserTest() {
 
         final String email = "kasimgul@gmail.com";
-
-        SiteUser user = new SiteUser();
-        user.setEmail(email);
-        user.setPassword("kasimgul123");
+        final SiteUser user = createBasicUser(email);
 
         Mockito.when(userRepository.findByEmail(email))
                 .thenReturn(Optional.of(user));
@@ -42,5 +59,12 @@ public class UserTest {
         else {
             fail();
         }
+    }
+
+    private SiteUser createBasicUser(String email) {
+        SiteUser user = new SiteUser();
+        user.setEmail(email);
+        user.setPassword("ksm123");
+        return user;
     }
 }
