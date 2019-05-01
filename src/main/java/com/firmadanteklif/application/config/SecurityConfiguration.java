920a510860d4c3b631e638f5383b6f4fe870f208
Kasim Gul <kasimgul@gmail.com>
2019-05-01 20:14:32 +0200

    Adding iletisim page and link

--- src/main/java/com/firmadanteklif/application/config/SecurityConfiguration.java
+++ src/main/java/com/firmadanteklif/application/config/SecurityConfiguration.java
@@ -37,6 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/user-kayit",
                         "/firma-giris",
                         "/firma-kayit",
+                        "/iletisim",
                         "/activation/**").permitAll()
                 .anyRequest()
                     .authenticated()
